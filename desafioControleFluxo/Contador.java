3251fba74f4cf133a421b9ff1e909810d3b95819
Kauan Oliveira <kauan.nana9@gmail.com>
2024-08-10 22:55:52 -0300

    Adicionado todo o comando do desafio, com tratamento de erros customizados

--- desafioControleFluxo/Contador.java
+++ desafioControleFluxo/Contador.java
@@ -1,28 +1,50 @@
+package desafioControleFluxo;
+
 import java.util.Scanner;
 
 public class Contador {
 	public static void main(String[] args) {
 		
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.;
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.??;
+		Scanner sc = new Scanner(System.in);
 		
+		//Obtenção de valores
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = sc.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = sc.nextInt();
+		
+		//Tratamento de exceções
 		try {
-			//chamando o método contendo a lógica de contagem
+		
+			//Chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch (? exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}catch (ParametrosInvalidosException e) {
+			
+			//Imprimindo a mensagem de erro customizada que aparecerá no terminal
+			System.err.println("O parâmetro UM precisa ser MENOR do que o parâmetro DOIS!!");
+			
+		}
+		
+		sc.close();
+	}
+	
+	//	Criação do método que será realizado toda a lógica da contagem, e caso aconteça um erro, volte uma mensagem de erro personalizada.
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+		//Validação, caso contenha o erro esperado, a "Exception" 'ParametrosInvalidosException' será chamada e executada
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+		}
+		
+		//Criado uma variável para ser realizado a contagem da diferença entre os parâmetros.
+		int contagem = parametroDois - parametroUm;
+		
+		//Execução do laço para impressão da contagem
+		System.out.printf("\nSerão %d números na contagem seguinte: \n", contagem);
+		for (int i = 1; i <= contagem; i++) {
+			System.out.printf("\nImprimindo o nº %d", i);
 		}
 		
 	}
-	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		
-		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
-	}
 }
-
---
+++ desafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+package desafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+}
